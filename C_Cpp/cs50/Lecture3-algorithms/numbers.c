9934b1bece4fdda7d9fe5e121cf06cc0b6a8810b
cuong desktop <honeychan0411@gmail.com>
2022-03-21 18:24:15 +0700

    starting cs50/Lecture3-algorithms

---
+++ C_Cpp/cs50/Lecture3-algorithms/numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(Void)
+{
+    int numbers[] = {4, 6, 8, 2, 7, 5, 0};
+
+    for (int i = 0; i < 7; i++)
+    {
+        if (numbers[i] == 0)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+}
