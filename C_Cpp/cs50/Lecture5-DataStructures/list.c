604367994d61094233f3e3866248272541917558
nguyen cuong <cuong0411@outlook.com>
2022-04-02 20:38:07 +0700

    malloc, free, realloc

--- C_Cpp/cs50/Lecture5-DataStructures/list.c
+++ C_Cpp/cs50/Lecture5-DataStructures/list.c
@@ -17,30 +17,51 @@ int main(void)
     list[0] = 1;
     list[1] = 2;
     list[2] = 3;
-
     /*
     *list = 1;
     *(list + 1) = 2;
     *(list + 2) = 3;
     */
 
-   int *temp = malloc(4 * sizeof(int));
-   if (temp == NULL)
-   {
-       free(list);
-       return 1;
-   }
-   
-   for (int i = 0; i < 3; i++)
-   {
-       temp[i] = list[i];
-   }
+//    // Allocate new array of size 4
+//    int *temp = malloc(4 * sizeof(int));
+//    if (temp == NULL)
+//    {
+//        free(list);
+//        return 1;
+//    }
+//     // Copy numbers from old array into new array
+//    for (int i = 0; i < 3; i++)
+//    {
+//        temp[i] = list[i];
+//    }
+
+   // Using realloc instead of malloc
+    int *temp = realloc(list, 4 * sizeof(int));
+    if (temp == NULL)
+    {
+        free(list);
+        return 1;
+    }
+
+   // Add fourth number to new array
    temp[3] = 4;
+
+    // Don't need this because of using realloc
+//    // free old array
+//    free(list);
+
+   // Remember new array
    list = temp;
+
+   // Print new array
    for (int i = 0; i < 4; i++)
    {
        printf("%i\n", list[i]);
    }
+
+   // Free new array
+   free(list);
    return 0;
 
 }
