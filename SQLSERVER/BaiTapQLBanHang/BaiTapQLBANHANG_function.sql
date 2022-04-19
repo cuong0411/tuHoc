9067a58c5750d3fba9600f4a277fb18ed366b651
nguyen cuong <cuong0411@outlook.com>
2022-04-19 20:36:39 +0700

    tao file cho bai tap qlbanhang, function

---
+++ SQLSERVER/BaiTapQLBanHang/BaiTapQLBANHANG_function.sql
@@ -0,0 +1,12 @@
+--tao cac function sau:
+-- 1.Viết hàm tính doanh thu của năm, với năm là tham số truyền vào.
+
+-- 2.Viết hàm tính doanh thu của tháng, năm, với tháng và năm là 2 tham số truyền vào.
+
+-- 3.Viết hàm tính doanh thu của khách hàng với mã khách hàng là tham số truyền vào.
+
+-- 4.Viết hàm tính tổng số lượng bán được cho từng mặt hàng theo tháng, năm nào đó.
+--Với mã hàng, tháng và năm là các tham số truyền vào, nếu tháng không nhập vào tức là tính tất cả các tháng.
+
+-- 5.Viết hàm tính lãi (giá bán – giá mua) * số lượng bán được cho từng mặt hàng,
+--với mã mặt hàng là tham số truyền vào. Nếu mã mặt hàng không truyền vào thì tính cho tất cả các mặt hàng.
